1c6f3f75d8c3d0453e2bbc745e2c8042cd8a51b2
y0hannes <yohanneset3@gmail.com>
2026-01-26 12:06:25 +0300

    refactor(shared): remove deprecated types and package files

--- apps/packages/shared/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './types/Project';
-export * from './types/Message';
--- apps/packages/shared/dist/types/Message.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Message {
-    _id?: string;
-    name: string;
-    email: string;
-    subject: string;
-    content: string;
-    createdAt?: Date | string;
-    updatedAt?: Date | string;
-}
