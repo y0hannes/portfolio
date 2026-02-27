f5fc03a3a7dde50073cc3c1614a63365cf4d35e6
y0hannes <yohanneset3@gmail.com>
2026-02-27 22:09:27 +0300

    feat: create experiance methods

--- apps/api/src/routes/route.ts
+++ apps/api/src/routes/route.ts
@@ -1,15 +1,29 @@
 import express from 'express';
-import { 
+import {
   getProjects,
   postProject,
   updateProject,
-  deleteProject
+  deleteProject,
 } from '../controllers/projectsController';
+
+import {
+  getExperiences,
+  postExperience,
+  updateExperience,
+  deleteExperience,
+} from '../controllers/experienceController';
+
+import {
+  getCertificates,
+  postCertificate,
+  updateCertificate,
+  deleteCertificate,
+} from '../controllers/certificateController';
 
 import {
   getMessages,
   createMessage,
-  deleteMessage
+  deleteMessage,
 } from '../controllers/messageController';
 
 import { login, authenticateAdmin } from '../controllers/authController';
@@ -19,11 +33,31 @@ const routes = express.Router();
 
 routes.post('/login', login);
 routes.get('/projects', getProjects);
-routes.post('/projects', authenticateAdmin, upload.single('image'), postProject);
-routes.put('/projects/:id', authenticateAdmin, upload.single('image'), updateProject);
+routes.post(
+  '/projects',
+  authenticateAdmin,
+  upload.single('image'),
+  postProject,
+);
+routes.put(
+  '/projects/:id',
+  authenticateAdmin,
+  upload.single('image'),
+  updateProject,
+);
 routes.delete('/projects/:id', authenticateAdmin, deleteProject);
 routes.get('/messages', authenticateAdmin, getMessages);
 routes.post('/messages', createMessage);
 routes.delete('/messages/:id', authenticateAdmin, deleteMessage);
+
+routes.get('/experiences', getExperiences);
+routes.post('/experiences', authenticateAdmin, postExperience);
+routes.put('/experiences/:id', authenticateAdmin, updateExperience);
+routes.delete('/experiences/:id', authenticateAdmin, deleteExperience);
+
+routes.get('/certificates', getCertificates);
+routes.post('/certificates', authenticateAdmin, postCertificate);
+routes.put('/certificates/:id', authenticateAdmin, updateCertificate);
+routes.delete('/certificates/:id', authenticateAdmin, deleteCertificate);
 
 export default routes;
---
+++ apps/types/Experience.ts
@@ -0,0 +1,9 @@
+export interface Experience {
+  _id?: string;
+  role: string;
+  company: string;
+  period: string;
+  description: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
